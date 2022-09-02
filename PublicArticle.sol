fabd4ed582a65719141e80e41a676c7991b6b09b
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-09-02 17:11:15 -0400

    Update PublicArticle.sol

--- PublicArticle.sol
+++ PublicArticle.sol
@@ -4,4 +4,8 @@ pragma solidity ^0.8.7;
 contract PublicArticle{
     string private articleName;
     string private articleText;
+    
+    uint public expires;
+    address public owner = msg.sender;
+    
 }
